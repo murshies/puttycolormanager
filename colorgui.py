d3654dd684b5ed5aa16742eebd31582a10a99ca2
Dan Bouffard <bouffard.daniel25@gmail.com>
2015-06-06 16:26:35 -0400

    Add the main non-button widgets to the interface. The listbox with all of the PuTTY session names still has sizing issues.

--- colorgui.py
+++ colorgui.py
@@ -1,24 +1,62 @@
 
+from collections import namedtuple
 import tkinter as tk
 
 import colorinterface
 
+ColorGroup = namedtuple('ColorGroup', 'red green blue')
+
+class ConfigDisplayWidget(tk.Frame):
+    def __init__(self, master, session_names):
+        tk.Frame.__init__(self, master)
+        self.master = master
+        self.session_names = session_names
+        
+        self.session_list = tk.Listbox(
+            self, selectmode=tk.EXTENDED)
+        
+        for name in self.session_names:
+            session_display_name = name.replace('%20', ' ')
+            self.session_list.insert(tk.END, session_display_name)
+
+        self.session_list.grid(row=0, column=0)
+
+class ColorValuesWidget(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        self.master = master
+        self.colorInputs = {}
+
+        self.canvas = tk.Canvas(self)
+        self.canvas.grid(row=0, column=0)
+
+        for num, color in enumerate(colorinterface.PUTTY_COLOR_ORDER):
+            nextLabel = tk.Label(self.canvas, text=color)
+            nextLabel.grid(row=num, column=0, rowspan=1)
+            redInput = tk.Entry(self.canvas, width=5)
+            blueInput = tk.Entry(self.canvas, width=5)
+            greenInput = tk.Entry(self.canvas, width=5)
+            redInput.grid(row=num, column=1, rowspan=1)
+            blueInput.grid(row=num, column=2, rowspan=1)
+            greenInput.grid(row=num, column=3, rowspan=1)
+            self.colorInputs[nextLabel] = ColorGroup(
+                redInput, greenInput, blueInput)
+
 class ColorInterface(tk.Frame):
     def __init__(self, master, session_names):
         tk.Frame.__init__(self, master)
-        self.session_names = session_names
+        self.master = master
         
-        # Initialize the session Listbox.
-        scrollbar = tk.Scrollbar(master, orient=tk.VERTICAL)
-        self.session_list = tk.Listbox(
-            master, selectmode=tk.EXTENDED, yscrollcommand=scrollbar.set)
-        scrollbar.config(command=self.session_list.yview)
-        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-        self.session_list.pack(side=tk.LEFT, fill=tk.Y, expand=1)
+        # Initialize the sub-widgets.
+        self.config_display = ConfigDisplayWidget(self, session_names)
+        self.color_values = ColorValuesWidget(self)
         
-        for name in session_names:
-            session_display_name = name.replace('%20', ' ')
-            self.session_list.insert(tk.END, session_display_name)
+        # Place the sub-widgets within the main frame.
+        self.config_display.grid(row=0, column=0)
+        self.color_values.grid(row=0, column=1)
+        
+        # Place the main widget in the top level widget.
+        self.grid(row=0)
         
 def main():
     '''Initialize the window and enter the Tkinter main loop.'''
