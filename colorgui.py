1769d4506ac79fb41789478d9cfc83eb315563c0
Dan Bouffard <bouffard.daniel25@gmail.com>
2015-06-07 17:05:28 -0400

    Create main interface without buttons.

--- colorgui.py
+++ colorgui.py
@@ -6,7 +6,7 @@ import colorinterface
 
 ColorGroup = namedtuple('ColorGroup', 'red green blue')
 
-class ConfigDisplayWidget(tk.Frame):
+class ConfigDisplayWidgetOld(tk.Frame):
     def __init__(self, master, session_names):
         tk.Frame.__init__(self, master)
         self.master = master
@@ -21,7 +21,50 @@ class ConfigDisplayWidget(tk.Frame):
 
         self.session_list.grid(row=0, column=0)
 
+class ConfigDisplayWidget(tk.Frame):
+    def __init__(self, master, session_names):
+        tk.Frame.__init__(self, master)
+        self.master = master
+        self.session_names = session_names
+        
+        self.session_list = tk.Listbox(
+            self, selectmode=tk.EXTENDED)
+        
+        for name in self.session_names:
+            session_display_name = name.replace('%20', ' ')
+            self.session_list.insert(tk.END, session_display_name)
+
+        self.session_list.pack(fill=tk.BOTH, expand=True)
+
+class ColorValuesFrame(tk.Frame):
+    def __init__(self, master, label_text):
+        tk.Frame.__init__(self, master)
+        
+        label = tk.Label(self, text=label_text)
+        label.pack(side=tk.LEFT, fill=tk.X)
+        
+        red_input = tk.Entry(self, width=5)
+        red_input.pack(side=tk.RIGHT, fill=tk.Y)
+        
+        green_input = tk.Entry(self, width=5)
+        green_input.pack(side=tk.RIGHT, fill=tk.Y)
+        
+        blue_input = tk.Entry(self, width=5)
+        blue_input.pack(side=tk.RIGHT, fill=tk.Y)
+
 class ColorValuesWidget(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        self.master = master
+        self.colorInputs = {}
+        
+        # Create the column for the inputs.
+        for num, color in enumerate(colorinterface.PUTTY_COLOR_ORDER):
+            next_input_group = ColorValuesFrame(self, color)
+            next_input_group.pack(fill=tk.X)
+            self.colorInputs[color] = next_input_group
+
+class ColorValuesWidgetOld(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
         self.master = master
@@ -42,6 +85,22 @@ class ColorValuesWidget(tk.Frame):
             self.colorInputs[nextLabel] = ColorGroup(
                 redInput, greenInput, blueInput)
 
+class ColorInterfaceOld(tk.Frame):
+    def __init__(self, master, session_names):
+        tk.Frame.__init__(self, master)
+        self.master = master
+        
+        # Initialize the sub-widgets.
+        self.config_display = ConfigDisplayWidget(self, session_names)
+        self.color_values = ColorValuesWidget(self)
+        
+        # Place the sub-widgets within the main frame.
+        self.config_display.grid(row=0, column=0, sticky=tk.N+tk.S)
+        self.color_values.grid(row=0, column=1)
+        
+        # Place the main widget in the top level widget.
+        self.grid(row=0)
+
 class ColorInterface(tk.Frame):
     def __init__(self, master, session_names):
         tk.Frame.__init__(self, master)
@@ -52,17 +111,14 @@ class ColorInterface(tk.Frame):
         self.color_values = ColorValuesWidget(self)
         
         # Place the sub-widgets within the main frame.
-        self.config_display.grid(row=0, column=0)
-        self.color_values.grid(row=0, column=1)
-        
-        # Place the main widget in the top level widget.
-        self.grid(row=0)
-        
+        self.config_display.pack(side=tk.LEFT, fill=tk.BOTH)
+        self.color_values.pack(side=tk.RIGHT)
+
 def main():
     '''Initialize the window and enter the Tkinter main loop.'''
     top = tk.Tk()
     interface = ColorInterface(top, colorinterface.getAllSessionNames())
-    
+    interface.pack()
     tk.mainloop()
 
 if __name__ == '__main__':
