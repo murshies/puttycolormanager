1d8f92a17257f16c79f6fd8dfc34a4052937a688
Dan Bouffard <bouffard.daniel25@gmail.com>
2015-06-07 17:52:08 -0400

    Add button frame and create callback stubs.

--- colorgui.py
+++ colorgui.py
@@ -53,12 +53,70 @@ class ColorInterface(tk.Frame):
         self.master = master
         
         # Initialize the sub-widgets.
-        self.config_display = ConfigDisplayWidget(self, session_names)
-        self.color_values = ColorValuesWidget(self)
+        display_frame = tk.Frame(self)
+        config_display = ConfigDisplayWidget(display_frame, session_names)
+        color_values = ColorValuesWidget(display_frame)
         
-        # Place the sub-widgets within the main frame.
-        self.config_display.pack(side=tk.LEFT, fill=tk.BOTH)
-        self.color_values.pack(side=tk.RIGHT)
+        # Place the sub-widgets within the display frame.
+        config_display.pack(side=tk.LEFT, fill=tk.BOTH)
+        color_values.pack(side=tk.RIGHT)
+        
+        # Initialize the button frame.
+        button_frame = ButtonFrame(self)
+        
+        # Add the display and button frames to the main frame.
+        display_frame.pack()
+        button_frame.pack()
+
+    def load_selected(self):
+        print('load selected')
+        
+    def load_from_file(self):
+        print('load from file')
+        
+    def apply_to_selected(self):
+        print('apply to selected')
+        
+    def save_to_file(self):
+        print('save to file')
+
+class ButtonFrame(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        self.master = master
+        
+        # Initialize the buttons.
+        load_selected = tk.Button(
+            self, text='Load Selected',
+            command=lambda: self._load_selected_callback())
+        load_selected.pack(side=tk.LEFT)
+        
+        load_from_file = tk.Button(
+            self, text='Load From File',
+            command=lambda: self._load_from_file_callback())
+        load_from_file.pack(side=tk.LEFT)
+        
+        apply_to_selected = tk.Button(
+            self, text='Apply To Selected',
+            command=lambda: self._apply_to_selected_callback())
+        apply_to_selected.pack(side=tk.LEFT)
+        
+        save_to_file = tk.Button(
+            self, text='Save To File',
+            command=lambda: self._save_to_file_callback())
+        save_to_file.pack(side=tk.LEFT)
+        
+    def _load_selected_callback(self):
+        self.master.load_selected()
+        
+    def _load_from_file_callback(self):
+        self.master.load_from_file()
+        
+    def _apply_to_selected_callback(self):
+        self.master.apply_to_selected()
+        
+    def _save_to_file_callback(self):
+        self.master.save_to_file()
 
 def main():
     '''Initialize the window and enter the Tkinter main loop.'''
