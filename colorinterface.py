d0acd359edd1965096304f0f68a55de1cbed35d2
Dan Bouffard <bouffard.daniel25@gmail.com>
2015-06-14 00:47:24 -0400

    Convert names in colorinterface.py to underscore format.

--- colorinterface.py
+++ colorinterface.py
@@ -57,94 +57,95 @@ WINDOWS_RESERVED = 0
 # The INI section name for color themes read from an INI file.
 COLOR_INI_SECTION_NAME = 'Colors'
 
-def unpackRegistryColor(regValue):
+def unpack_registry_color(reg_value):
     '''Given the value of a color read from the registry, return a list of
-    integers of the RGB values. "regValue" will be a tuple of 2 items,
+    integers of the RGB values. "reg_value" will be a tuple of 2 items,
     containing the value of the registry item (a string in our case), and an
     integer representing the item type (1 in our case). The colors in the
     registry are comma-separated (e.g. 255,255,255).'''
-    return unpackColor(regValue[0])
+    return unpack_color(reg_value[0])
 
-def unpackColor(colorVal):
+def unpack_color(color_val):
     '''Given a color in the form of a comma-separated RGB string, return a
     tuple of integers. Whitspace is allowed between integers.'''
-    return tuple([int(x.strip()) for x in colorVal.split(',')])
+    return tuple([int(x.strip()) for x in color_val.split(',')])
 
-def packRegistryColors(colorList):
+def pack_registry_colors(color_list):
     '''Converts a color in the form of an RGB list of integers into the format
     expected by PuTTY (a comma-separated list of integers in the form of a
     string).'''
-    return ','.join([str(x) for x in colorList])
+    return ','.join([str(x) for x in color_list])
 
-def readSessionColors(sessionName):
+def read_session_colors(session_name):
     '''Read all of the color values for the PuTTY session with name
-    "sessionName".'''
-    fullSessionName = BASE_PUTTY_PATH + sessionName
-    colorsList = []
+    "session_name".'''
+    full_session_name = BASE_PUTTY_PATH + session_name
+    colors_list = []
     
     with winreg.OpenKey(winreg.HKEY_CURRENT_USER,
-                         fullSessionName) as session:
-        for colorNumber in range(len(PUTTY_COLOR_ORDER)):
-            regColorName = 'Colour{0}'.format(colorNumber)
-            regValue = winreg.QueryValueEx(session, regColorName)
-            colors = unpackRegistryColor(regValue)
-            colorsList.append(colors)
+                        full_session_name) as session:
+        for color_number in range(len(PUTTY_COLOR_ORDER)):
+            reg_color_name = 'Colour{0}'.format(color_number)
+            reg_value = winreg.QueryValueEx(session, reg_color_name)
+            colors = unpack_registry_color(reg_value)
+            colors_list.append(colors)
     
-    return colorsList
+    return colors_list
 
-def writeSessionColors(sessionName, colorList):
-    '''Given the name of a session "sessionName" and a list of RGB integer
-    lists, set colorList as the colors for sessionName.'''
-    fullSessionName = BASE_PUTTY_PATH + sessionName
+def writeSessionColors(session_name, color_list):
+    '''Given the name of a session "session_name" and a list of RGB integer
+    lists, set color_list as the colors for session_name.'''
+    full_session_name = BASE_PUTTY_PATH + session_name
     
-    with winreg.OpenKey(winreg.HKEY_CURRENT_USER, fullSessionName,
-                         WINDOWS_RESERVED, winreg.KEY_WRITE) as session:
-        for colorNumber, colorVal in enumerate(colorList):
-            regColorName = 'Colour{0}'.format(colorNumber)
-            packedColor = packRegistryColors(colorVal)
-            winreg.SetValueEx(session, regColorName, WINDOWS_RESERVED,
-                               PUTTY_REG_COLOR_TYPE, packedColor)
+    with winreg.OpenKey(winreg.HKEY_CURRENT_USER, full_session_name,
+                        WINDOWS_RESERVED, winreg.KEY_WRITE) as session:
+        for color_number, color_val in enumerate(color_list):
+            reg_color_name = 'Colour{0}'.format(color_number)
+            packed_color = pack_registry_colors(color_val)
+            winreg.SetValueEx(session, reg_color_name, WINDOWS_RESERVED,
+                              PUTTY_REG_COLOR_TYPE, packed_color)
 
-def getAllSessionNames():
+def get_all_session_names():
     '''Get the name of all PuTTY sessions.'''
-    sessionNames = []
+    session_names = []
     
     with winreg.OpenKey(winreg.HKEY_CURRENT_USER, BASE_PUTTY_PATH) as base:
         try:
             i = 0
             while True:
-                sessionNames.append(winreg.EnumKey(base, i))
+                session_names.append(winreg.EnumKey(base, i))
                 i += 1
         except WindowsError:
             pass
     
-    return sessionNames
+    return session_names
 
-def readColorsFromINI(iniName):
-    '''Given the name of an INI file "iniName", create a list of RGB tuples
-    containing the colors found in iniName.'''
-    colorConfig = configparser.SafeConfigParser()
-    colorConfig.read(iniName)
-    colorDict = colorConfig[COLOR_INI_SECTION_NAME]
-    colorList = []
+def read_colors_from_INI(ini_name):
+    '''Given the name of an INI file "ini_name", create a list of RGB tuples
+    containing the colors found in ini_name.'''
+    color_config = configparser.SafeConfigParser()
+    color_config.read(ini_name)
+    color_dict = color_config[COLOR_INI_SECTION_NAME]
+    color_list = []
     
-    for colorNumber, colorName in enumerate(PUTTY_COLOR_ORDER):
-        regColorName = 'Colour{0}'.format(colorNumber)
-        currColor = PUTTY_DEFAULT_COLORS[colorNumber]
-        if regColorName in colorDict:
-            currColor = unpackColor(colorDict[regColorName])
-        elif colorName in colorDict:
-            currColor = unpackColor(colorDict[colorName])
+    for color_number, color_name in enumerate(PUTTY_COLOR_ORDER):
+        reg_color_name = 'Colour{0}'.format(color_number)
+        curr_color = PUTTY_DEFAULT_COLORS[color_number]
+        if reg_color_name in color_dict:
+            curr_color = unpack_color(color_dict[reg_color_name])
+        elif color_name in color_dict:
+            curr_color = unpack_color(color_dict[color_name])
         else:
             logging.warning(('No value for {0} ({1}) found in {2} - using the '
-                            'default value {3}').format(
-                                regColorName, colorName, iniName, currColor))
-        colorList.append(currColor)
+                             'default value {3}').format(
+                                reg_color_name, color_name, ini_name,
+                                curr_color))
+        color_list.append(curr_color)
     
-    return colorList
+    return color_list
 
 def main():
-    print(readColorsFromINI('solarized_dark.ini'))
+    print(read_colors_from_INI('solarized_dark.ini'))
 
 if __name__ == '__main__':
     main()
